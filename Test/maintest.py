7d83e812ca7d49d8345b2489456e796bd64ae784
HYC <yunchaohe@gmail.com>
2013-10-14 19:17:28 +0800

    python: pickle try-except-finally locals() sys.stdout

--- Test/maintest.py
+++
@@ -1,5 +0,0 @@
-import mytest
-#movies=["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,["Graham Chapman",["Michael Palin",
-#"John Cleese","Terry Gilliam","Eric Idle","Terry Jones"]]]
-names=['John','Eric',['Cleese','Idle'],'Michael',['Palin']]
-mytest.print_lol(names,True,0)
